cf8f36b69a694bc25439ad32d609d67109850cb2
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-13 00:29:28 +0100

    Update 2-data-types.sql

--- le-wagon-sql/2-data-types.sql
+++ le-wagon-sql/2-data-types.sql
@@ -1,1 +1,20 @@
+/* Covers: CAST, 
+Provided table :  circle_stock_ok
+*/
+
+-- Check the Primary key
+
+SELECT 
+  DISTINCT MODEL_ID
+FROM `wagon-portfolio.course14.circle_stock`
+
+-- Checking the schema of the circle_stock table, some columns are in a wrong type. I used the SAFE_CAST function to avoir an incorrect "-----" value in the price column
+
+SELECT MODEL_ID, MODEL_TYPE,MODEL_NAME,COLOR, 
+  CAST(STOCK AS INT64) as stock,
+  CAST(IN_STOCK AS BOOLEAN) as in_stock,
+  CAST(DATE_CREATION AS DATE) AS date_creation,
+  CAST(STOCK_DAYS AS FLOAT64) AS stock_days,
+  SAFE_CAST(PRICE AS FLOAT64) AS price_formated,
+FROM `wagon-portfolio.course14.circle_stock_ok`
 
