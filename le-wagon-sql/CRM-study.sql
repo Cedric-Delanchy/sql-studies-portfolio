fdfda4afdfa1cb92b461dc10527906ccac24a246
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-13 00:58:59 +0100

    Update CRM-study.sql

--- le-wagon-sql/CRM-study.sql
+++ le-wagon-sql/CRM-study.sql
@@ -10,7 +10,30 @@
 - turnover
 */
 
--- First exploration shows a lot of null values. I start by identifying a Primary Key
+-- First exploration shows a lot of null values. I start by identifying if the ID is the Primary Key
+
+SELECT journey_id,
+      ,COUNT(*) AS nb_lignes
+FROM `table`
+GROUP BY journey_id
+ORDER BY 2 DESC;
+
+-- Search for the largest email campaign
+
+SELECT journey_id,
+  journey_name,
+  sent_nb
+FROM `course14.gwz_mail`
+ORDER BY sent_nb DESC
+
+-- Identify campaigns generating more than 10k openings
 
 SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb
 FROM `course14.gwz_mail`
+WHERE opening_nb > 10000 
+ORDER BY opening_nb DESC
+
+
