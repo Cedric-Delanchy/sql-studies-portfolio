9932ac2e8f074063bae1acd8c86fdaaa4d585983
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-15 20:36:37 +0100

    Update 3-CRM-study.sql

--- le-wagon-sql/3-CRM-study.sql
+++ le-wagon-sql/3-CRM-study.sql
@@ -9,6 +9,8 @@ Table has 6 columns and 136 rows:
 - opening_nb
 - click_nb
 - turnover
+
+Topics: CALCULATION, ROUND, SAFE DIVIDE, LIKE/NOT LIKE, WILDCARDS
 */
 
 -- First exploration shows a lot of null values. I start by identifying if the ID is the Primary Key
@@ -36,5 +38,84 @@ SELECT DISTINCT journey_id,
 FROM `course14.gwz_mail`
 WHERE opening_nb > 10000 
 ORDER BY opening_nb DESC
+
+-- Identify the 10 most clicked campaigns
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb
+FROM `course14.gwz_mail`
+ORDER BY click_nb DESC
+LIMIT 10
+
+-- Check a specific campaign performance
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "210707_nl_happyhour%"
+
+-- Select only Belgium campaign filtering on the campaign name
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "%nlbe%"
+ORDER BY sent_nb DESC
+
+-- Check newsleters from France based on campaign names while naming contains "_nl_" for France and "_nlbe_" for Belgium
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "%_nl_%"
+  AND journey_name NOT LIKE "%nlbe%"
+ORDER BY sent_nb DESC
+
+-- Get the opening rate, click rate, ctr and turnover per mille
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb,
+  ROUND((SAFE_DIVIDE(opening_nb, sent_nb)*100),1) AS opening_rate,
+  ROUND((SAFE_DIVIDE(click_nb, sent_nb)*100),1) AS click_rate,
+  ROUND((click_nb/opening_nb*100),1) AS ctr,
+  ROUND((turnover/sent_nb*1000),1) AS turnover_per_mille
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "%_nl_%"
+  AND journey_name NOT LIKE "%nlbe%"
+ORDER BY sent_nb DESC
+
+-- Get a specific email test with 2 versions based on the campaign name
+
+SELECT DISTINCT journey_id,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb,
+  ROUND((SAFE_DIVIDE(opening_nb, sent_nb)*100),1) AS opening_rate,
+  ROUND((SAFE_DIVIDE(click_nb, sent_nb)*100),1) AS click_rate,
+  ROUND((click_nb/opening_nb*100),1) AS ctr,
+  ROUND((turnover/sent_nb*1000),1) AS turnover_per_mille
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "%_nl_%"
+  AND journey_name NOT LIKE "%nlbe%"
+  AND journey_name LIKE "%happyhour%"
+ORDER BY sent_nb DESC
+
+
 
 
