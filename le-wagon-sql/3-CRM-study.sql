cf788999562b876c4dfc89dacf53ed7ffb6125b5
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-16 16:47:38 +0100

    Update 3-CRM-study.sql

--- le-wagon-sql/3-CRM-study.sql
+++ le-wagon-sql/3-CRM-study.sql
@@ -10,7 +10,7 @@ Table has 6 columns and 136 rows:
 - click_nb
 - turnover
 
-Topics: CALCULATION, ROUND, SAFE DIVIDE, LIKE/NOT LIKE, WILDCARDS
+Topics: CALCULATION, ROUND, SAFE DIVIDE, LIKE/NOT LIKE, WILDCARDS, CAST, CONCAT, SUBSTR
 
 */
 
@@ -117,6 +117,26 @@ WHERE journey_name LIKE "%_nl_%"
   AND journey_name LIKE "%happyhour%"
 ORDER BY sent_nb DESC
 
+-- Extraire la date du nom de la campagne pour alimenter une colonne Date
+      
+SELECT DISTINCT journey_id,
+  CAST(CONCAT(20,SUBSTR(journey_name, 1, 2),'-',
+    SUBSTR(journey_name, 3, 2),'-',
+    SUBSTR(journey_name, 5, 2)) AS DATE) AS date_date,
+  journey_name,
+  sent_nb,
+  opening_nb,
+  click_nb,
+  ROUND((SAFE_DIVIDE(opening_nb, sent_nb)*100),1) AS opening_rate,
+  ROUND((SAFE_DIVIDE(click_nb, sent_nb)*100),1) AS click_rate,
+  ROUND((click_nb/opening_nb*100),1) AS ctr,
+  ROUND((turnover/sent_nb*1000),1) AS turnover_per_mille
+FROM `course14.gwz_mail`
+WHERE journey_name LIKE "%_nl_%"
+  AND journey_name NOT LIKE "%nlbe"
+  AND journey_name NOT LIKE "%COPY%"
+  AND journey_name NOT LIKE "2108_nl_marketplace_1"
+ORDER BY date_date DESC
 
 
 
