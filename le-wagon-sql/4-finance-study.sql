3ff7effd931c22a271a431a438001603caabb8ba
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-17 12:19:31 +0100

    Update 4-finance-study.sql

--- le-wagon-sql/4-finance-study.sql
+++ le-wagon-sql/4-finance-study.sql
@@ -1,1 +1,53 @@
+/* We are working on 3 different Finance tables.
+
+- Products: contains the listing of product ids and purchase prices
+- Sales: order datas with:
+  - Date
+  - orders_id
+  - products_id
+  - turnover
+  - qty
+- ship: shipping informations with:
+  - orders_id
+  - shipping_fee
+  - log_cost
+  - ship_cost
+*/
+
+-- I start by identifying each primary keys
+
+SELECT products_id,
+  COUNT(*) AS nb_lignes
+FROM `course16.gwz_product`
+GROUP BY products_id
+ORDER BY 2 DESC;
+
+SELECT orders_id,
+  COUNT(*) AS nb_lignes
+FROM `course16.gwz_ship`
+GROUP BY orders_id
+ORDER BY 2 DESC;
+
+SELECT orders_id,
+  COUNT(*) AS nb_lignes
+FROM `course16.gwz_sales`
+GROUP BY orders_id
+ORDER BY 2 DESC; 
+
+-- order id is not the primary key for SALES as it is duplicated. I check if it could be orders id and products ids associated
+
+SELECT orders_id,
+  products_id,
+  COUNT(*) AS nb
+FROM `course16.gwz_sales`
+GROUP BY orders_id,
+  products_id
+HAVING nb>=2
+
+
+
+
+
+
+
 
