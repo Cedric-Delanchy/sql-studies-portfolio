97d949e46774c617cc748cfa4c17c05de792e27b
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-18 20:27:23 +0100

    Update crud-study.sql

--- le-wagon-sql/crud-study.sql
+++ le-wagon-sql/crud-study.sql
@@ -1,1 +1,58 @@
+/* Practice around some basic CRUD operations
+
+QUERY: 
+*/
+
+-- Creation of a customer table
+
+CREATE TABLE `course19.gwz_customers`(
+  id INT64,
+  name STRING,
+  surname STRING,
+  creation_date DATE,
+  number_of_orders INT64,
+  total_turnover FLOAT64
+)
+
+-- insert values in the customer table
+
+INSERT INTO `course19.gwz_customers`
+VALUES
+  (1,"Paul","Mochkovitch","2020-05-12",4,246.21),
+  (2,"Maris","Dupuis","2021-08-07",1,54.63),
+  (3,"George","Smith","2021-09-23",2,186.21),
+  (6,"David","Martin","2022-09-02",2,306.99),
+  (4,"Samantha","Davis","2022-04-12",3,162.1),
+  (5,"Paul","Belmond","2022-08-28",0,0.0)
+
+-- Update some datas
+
+UPDATE `course19.gwz_customers`
+SET 
+  number_of_orders = 3,
+  total_turnover = 278.21
+WHERE id = 3
+
+-- Delete a customer
+
+DELETE
+FROM `course19.gwz_customers`
+WHERE id = 2
+
+--------------------------------------------------------------------------------------------------------
+
+-- Working on a CRM campaigns table, we want to delete belgian campaigns base on the name of the campaign
+
+DELETE
+FROM `course19.gwz_mail`
+WHERE journey_name LIKE "%nlbe%"
+
+-- Update a campaign name using SET
+
+UPDATE `course19.gwz_mail`
+SET journey_name = "210721_nl_hh_midi"
+WHERE journey_name = "COPY_OF_210721_nl_hh_midi"
+
+
+
 
