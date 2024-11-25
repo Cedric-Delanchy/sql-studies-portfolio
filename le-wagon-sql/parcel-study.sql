4a17aa2b7d424e69d983adf748c302dd22e4e9d4
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-11-25 20:19:38 +0100

    Update parcel-study.sql

--- le-wagon-sql/parcel-study.sql
+++ le-wagon-sql/parcel-study.sql
@@ -18,5 +18,22 @@ cc_parcel_product:
 - model_name: the name of the product model in the parcel
 - qty: the quantity of the specific product model in a given parcel
 
-
+QUERY: 
 */
+
+-- Identify PK cc_parcel
+
+SELECT parcel_id,
+  COUNT(*) AS nb
+FROM `course15.cc_parcel`
+GROUP BY parcel_id
+ORDER BY 2 DESC
+
+-- Identify PK cc_parcel_product
+
+SELECT parcel_id,
+  model_name,
+  COUNT(*) AS nb
+FROM `course15.cc_parcel_product`
+GROUP BY parcel_id, model_name
+ORDER BY 2 DESC
