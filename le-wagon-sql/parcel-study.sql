206ca43d75f454ec54ad6126ff150544a0028e82
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-12-04 19:27:13 +0100

    Update parcel-study.sql

--- le-wagon-sql/parcel-study.sql
+++ le-wagon-sql/parcel-study.sql
@@ -37,3 +37,33 @@ SELECT parcel_id,
 FROM `course15.cc_parcel_product`
 GROUP BY parcel_id, model_name
 ORDER BY 2 DESC
+
+-- Format date colums and create a status column based on the date values. Create a new cc_parcel_kpi table
+
+SELECT parcel_id,
+  parcel_tracking,
+  transporter,
+  priority,
+  PARSE_DATE('%B %e, %Y', date_purchase) AS date_purchase,
+  PARSE_DATE('%B %e, %Y', date_shipping) AS date_shipping,
+  PARSE_DATE('%B %e, %Y', date_delivery) AS date_delivery,
+  PARSE_DATE('%B %e, %Y', date_cancelled) AS date_cancelled,
+  CASE
+    WHEN date_cancelled IS NOT NULL THEN "Cancelled"
+    WHEN date_shipping IS NULL THEN "In Progress"
+    WHEN date_delivery IS NULL THEN "In Transit"
+    WHEN date_delivery IS NOT NULL THEN "Delivered"
+    ELSE NULL
+  END AS status
+FROM `course15.cc_parcel`
+
+-- Add expedition, transport and delivery times columns
+
+/* SELECT *,
+  DATE_DIFF(date_shipping,date_purchase,DAY) AS expedition_time,
+  DATE_DIFF(date_delivery,date_shipping,DAY) AS transport_time,
+  DATE_DIFF(date_delivery,date_purchase,DAY) AS delivery_time,
+FROM `course15.cc_parcel_kpi` 
+*/
+
+
