2b7f3fa2bb168bdc9b6a696c119dc3ac71f70bf8
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-12-17 20:08:32 +0100

    Update parcel-study.sql

--- le-wagon-sql/parcel-study.sql
+++ le-wagon-sql/parcel-study.sql
@@ -38,32 +38,77 @@ FROM `course15.cc_parcel_product`
 GROUP BY parcel_id, model_name
 ORDER BY 2 DESC
 
--- Format date colums and create a status column based on the date values. Create a new cc_parcel_kpi table
+/*
+- Format date colums and create a status column based on the date values. 
+- Add expedition, transport and delivery times columns based on dates.
+- Save the query in a cc_parcel_kpi table.
+*/
 
 SELECT parcel_id,
   parcel_tracking,
   transporter,
   priority,
-  PARSE_DATE('%B %e, %Y', date_purchase) AS date_purchase,
-  PARSE_DATE('%B %e, %Y', date_shipping) AS date_shipping,
-  PARSE_DATE('%B %e, %Y', date_delivery) AS date_delivery,
-  PARSE_DATE('%B %e, %Y', date_cancelled) AS date_cancelled,
+  PARSE_DATE("%B %e, %Y", date_purchase) AS date_purchase,
+  PARSE_DATE("%B %e, %Y", date_shipping) AS date_shipping,
+  PARSE_DATE("%B %e, %Y", date_delivery) AS date_delivery,
+  PARSE_DATE("%B %e, %Y", date_cancelled) AS date_cancelled,
   CASE
-    WHEN date_cancelled IS NOT NULL THEN "Cancelled"
-    WHEN date_shipping IS NULL THEN "In Progress"
-    WHEN date_delivery IS NULL THEN "In Transit"
-    WHEN date_delivery IS NOT NULL THEN "Delivered"
+    WHEN date_cancelled IS NOT NULL THEN 'Cancelled'
+    WHEN date_shipping IS NULL THEN 'In Progress'
+    WHEN date_delivery IS NULL THEN 'In Transit'
+    WHEN date_delivery IS NOT NULL THEN 'Delivered'
     ELSE NULL
-  END AS status
+  END AS status,
+  DATE_DIFF(PARSE_DATE("%B %e, %Y", date_shipping),PARSE_DATE("%B %e, %Y", date_purchase),DAY) AS expedition_time,
+  DATE_DIFF(PARSE_DATE("%B %e, %Y", date_delivery),PARSE_DATE("%B %e, %Y", date_shipping),DAY) AS transport_time,
+  DATE_DIFF(PARSE_DATE("%B %e, %Y", date_delivery),PARSE_DATE("%B %e, %Y", date_purchase),DAY) AS delivery_time,
 FROM `course15.cc_parcel`
 
--- Add expedition, transport and delivery times columns
+-- Create a Macro KPI table for the team and build on it
 
-/* SELECT *,
-  DATE_DIFF(date_shipping,date_purchase,DAY) AS expedition_time,
-  DATE_DIFF(date_delivery,date_shipping,DAY) AS transport_time,
-  DATE_DIFF(date_delivery,date_purchase,DAY) AS delivery_time,
-FROM `course15.cc_parcel_kpi` 
-*/
+SELECT
+  COUNT(*) AS parcel_quantities,
+  ROUND(AVG(expedition_time),2) AS avg_shipping,
+  ROUND(AVG(transport_time),2) AS avg_transport_time,
+  ROUND(AVG(delivery_time),2) AS avg_delivery_time
+FROM `course15.cc_parcel_kpi`
 
+-- By transporters
+
+SELECT transporter,
+  COUNT(*) AS parcel_quantities,
+  ROUND(AVG(expedition_time),2) AS avg_shipping,
+  ROUND(AVG(transport_time),2) AS avg_transport_time,
+  ROUND(AVG(delivery_time),2) AS avg_delivery_time
+FROM `course15.cc_parcel_kpi`
+GROUP BY transporter
+
+-- By priorities
+  
+SELECT
+  priority,
+  COUNT(*) AS nb_parcel,
+  ROUND(AVG(expedition_time),2) AS expedition_time,
+  ROUND(AVG(transport_time),2) AS transport_time,
+  ROUND(AVG(delivery_time),2) AS delivery_time,
+  ROUND(SAFE_DIVIDE(AVG(transport_time),AVG(delivery_time)),2) AS ratio_transport_delivery
+FROM `course15.cc_parcel_kpi`
+GROUP BY priority
+
+-- Create a month_purchase column in our cc_parcel_kpi table
+
+SELECT *,
+  EXTRACT(MONTH FROM date_purchase) AS month_purchase
+FROM `course15.cc_parcel_kpi`
+
+-- Check the evolution accross months
+
+SELECT month_purchase,
+  COUNT(*) AS number_of_parcels,
+  ROUND(AVG(expedition_time),2) AS avg_expedition_time,
+  ROUND(AVG(transport_time),2) AS avg_transport_time,
+  ROUND(AVG(delivery_time),2) AS avg_delivery_time,
+FROM `course15.cc_parcel_kpi_new`
+GROUP BY month_purchase
+ORDER BY month_purchase
 
