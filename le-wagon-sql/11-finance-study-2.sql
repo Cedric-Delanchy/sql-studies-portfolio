91684bb1d015f5e5aff49532ac421b889e3beef8
Cédric Delanchy <61972885+Cedric-Delanchy@users.noreply.github.com>
2024-12-19 20:32:11 +0100

    Create 11-finance-study-2.sql

---
+++ le-wagon-sql/11-finance-study-2.sql
@@ -0,0 +1,21 @@
+/*
+Objectives: Practice JOINS
+
+3 tables : Product, Sales, Ship
+
+Inputs
+---
+
+**Product**
+Primary key - products_id
+Foreign_key - product_id in gwz_sales
+
+**Sales**
+Primary key - orders_id, products_id
+Foreign_keys - orders_id in gwz_ship and product_id in gwz_product
+
+**Ship**
+Primary key - orders_id
+Foreign_key - orders_id in gwz_sales
+
+*/
